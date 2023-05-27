73012f69c088a6b65e8d77d648501cb5ac3751e5
ENigmusSS <128493758+ENigmusSS@users.noreply.github.com>
2023-05-27 17:17:41 +0300

    hw12

---
+++ src/main/java/ua/holovchenko/hw12/task3/Ex1.java
@@ -0,0 +1,8 @@
+package ua.holovchenko.hw12.task3;
+
+public class Ex1 extends Exception{
+    public Ex1(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/ua/holovchenko/hw12/task3/Task3.java
@@ -0,0 +1,21 @@
+package ua.holovchenko.hw12.task3;
+
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+        try {
+            exRoulette();
+        }
+        catch (Ex1 | Ex2 | Ex3 ex) {System.out.println(ex.getMessage());}
+    }
+
+    public static void exRoulette() throws Ex1, Ex2, Ex3 {
+        Random rnd = new Random();
+        switch (rnd.nextInt(1, 4)) {
+            case 1 -> throw new Ex1("Ex1");
+            case 2 -> throw new Ex2("Ex2");
+            case 3 -> throw new Ex3("Ex3");
+        }
+    }
+}
