c23572d591bcb78dcbde4609d67f7474a87ba667
ENigmusSS <128493758+ENigmusSS@users.noreply.github.com>
2023-07-18 23:08:10 +0300

    hw21 (string fields only init)

--- src/main/java/ua/holovchenko/hw15/task1/ListIteration.java
+++ src/main/java/ua/holovchenko/hw15/task1/ListIteration.java
@@ -43,7 +43,9 @@ public class ListIteration {
     }
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
-            list.add("a"); list.add("b"); list.add("c");
+            list.add("a");
+            list.add("b");
+            list.add("c");
         forCyclePrint(list);
         forEachCyclePrint(list);
         whileCyclePrint(list);
